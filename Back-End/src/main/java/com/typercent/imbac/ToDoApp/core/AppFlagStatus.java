0a7c96f41927e720e63f0b6f3ec532c17dc43d76
imbac <imbac.smil3@gmail.com>
2021-07-06 22:08:41 +0700

    add core to project

---
+++ Back-End/src/main/java/com/typercent/imbac/ToDoApp/core/AppFlagStatus.java
@@ -0,0 +1,11 @@
+package com.typercent.imbac.ToDoApp.core;
+
+public class AppFlagStatus {
+
+    AppFlagStatus() {};
+
+    public static class IS_COMPLETED_FLAG {
+        public static final Boolean N = false;
+        public static final Boolean Y = true;
+    }
+}
---
+++ Back-End/src/main/java/com/typercent/imbac/ToDoApp/core/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.typercent.imbac.ToDoApp.core;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "CREATED_DATE", nullable = false)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    protected LocalDateTime createdDate;
+
+    @Column(name = "UPDATED_DATE")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    protected LocalDateTime updateddDate;
+}
