2ccdc3d755517ac89997417ff87fbd7d2693e8eb
imbac <imbac.smil3@gmail.com>
2021-07-06 22:08:54 +0700

    add exception to project

---
+++ Back-End/src/main/java/com/typercent/imbac/ToDoApp/exception/DataNotFoundException.java
@@ -0,0 +1,17 @@
+package com.typercent.imbac.ToDoApp.exception;
+
+import lombok.Data;
+
+@Data
+public class DataNotFoundException extends RuntimeException {
+    private String errorCode = "ERROR001";
+    private String error;
+
+    public DataNotFoundException(String msg) {
+        super(msg);
+    }
+
+//    public DataNotFoundException(String msg, String error) {
+//        super(msg);
+//    }
+}
---
+++ Back-End/src/main/java/com/typercent/imbac/ToDoApp/exception/ErrorExceptionHandling.java
@@ -0,0 +1,30 @@
+package com.typercent.imbac.ToDoApp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorExceptionHandling {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorExceptionResponse> handleBaseException(Exception e) {
+        ErrorExceptionResponse response = ErrorExceptionResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .errorCode("ERR000")
+                .error(e.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    public ResponseEntity<ErrorExceptionResponse> handleDataNotFoundException(DataNotFoundException e) {
+        ErrorExceptionResponse response = ErrorExceptionResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .errorCode(e.getErrorCode())
+                .error(e.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.EXPECTATION_FAILED);
+    }
+}
