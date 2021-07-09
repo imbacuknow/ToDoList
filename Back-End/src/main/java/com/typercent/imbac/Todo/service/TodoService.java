6a45a0ee65a2697752d8c9a8aa47d3ea6a7e90bd
imbac <imbac.smil3@gmail.com>
2021-07-09 21:11:22 +0700

    update TodoService

--- Back-End/src/main/java/com/typercent/imbac/Todo/service/TodoService.java
+++ Back-End/src/main/java/com/typercent/imbac/Todo/service/TodoService.java
@@ -15,9 +15,9 @@ import java.util.List;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class ToDoService {
+public class TodoService {
 
-    private final TodoRepo toDoRepo;
+    private final TodoRepo todoRepo;
 
     // -------------- create --------------
     public Todo createNewToDo(TodoDTO request){
@@ -25,41 +25,55 @@ public class ToDoService {
         Todo toDo = new Todo();
         toDo.setCreatedDate(LocalDateTime.now());
         toDo.setTitle(request.getTitle());
-        return this.toDoRepo.save(toDo);
+        return this.todoRepo.save(toDo);
     }
 
     // -------------- read --------------
     public List<Todo> retrieveAllToDoList() {
         log.info("retrieveAllToDoList");
-        return this.toDoRepo.findAll();
+        return this.todoRepo.findAll();
     }
 
     public List<Todo> retrieveUncompleteToDoList() {
         log.info("retrieveUncompleteToDoList");
-        return this.toDoRepo.findByIsComplete(false);
+        return this.todoRepo.findByIsComplete(false);
     }
 
     public List<Todo> retrieveCompleteToDoList() {
         log.info("retrieveCompleteToDoList");
-        return this.toDoRepo.findByIsComplete(true);
+        return this.todoRepo.findByIsComplete(true);
     }
 
     // -------------- update --------------
     public Todo updateToDo(TodoDTO request) {/**/
         log.info("updateNewToDo: {}", request);
-        Todo todo = this.toDoRepo.findById(request.getId())
+        Todo todo = this.todoRepo.findById(request.getId())
                 .orElseThrow(() -> new DataNotFoundException("Can't update 'Cause this id " + request.getId() + " does not exist in Todo"));
         todo.setTitle(request.getTitle());
         todo.setUpdateddDate(LocalDateTime.now());
-        return this.toDoRepo.save(todo);
+        return this.todoRepo.save(todo);
     }
 
     public Todo updateToDoStatus(Long id) {
         log.info("updateNewToDo: id={}", id);
-        Todo todo = this.toDoRepo.findById(id)
+        Todo todo = this.todoRepo.findById(id)
                 .orElseThrow(() -> new DataNotFoundException("Can't update 'Cause this id " + id + " does not exist in Todo"));
         todo.setIsComplete(AppFlag.IS_COMPLETED_FLAG.Y);
         todo.setUpdateddDate(LocalDateTime.now());
-        return this.toDoRepo.save(todo);
+        return this.todoRepo.save(todo);
+    }
+
+    // -------------- delete --------------
+    public void deleteTodo(Long id) {
+        log.info("deleteTodo: id={}", id);
+        Todo todo = this.todoRepo.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("Can't delete 'Cause this id " + id + " does not exist in Todo"));
+        this.todoRepo.deleteById(id);
+    }
+
+    public void deleteAllCompleteTodo() {
+        log.info("deleteAllCompleteTodo");
+        List<Todo> todoList = this.todoRepo.findByIsComplete(true);
+        this.todoRepo.deleteAll(todoList);
     }
 }
